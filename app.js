3145af4a4312894dec93f4becc18b195db7f9794
moh-mme <mohammed19nattek@gmail.com>
2025-01-15 20:17:08 +0300

    del comment

--- app.js
+++ app.js
@@ -27,23 +27,3 @@ btn.addEventListener("click",function (){
     }
 });
 
-// let req = new XMLHttpRequest();
-// req.open("GET","https://api.github.com/users/moh-mme/repos");
-// req.send();
-// console.log(req);
-// req.onreadystatechange = function (){
-//     console.log(req.readyState);
-//     console.log(req.status);
-//     if(this.readyState === 4 && this.status === 200){
-//         let jsData = JSON.parse(this.responseText);
-//         console.log(jsData);
-//         for(let i = 0; i<= jsData.length; i++){
-//             // let div = document.createElement("div");
-//             // let repoName = document.createElement("h2");
-//             // repoName.innerHTML = jsData[i].name;
-//             // div.appendChild(repoName);
-//             // document.body.appendChild(div);
-//         }
-//     }
-// }
-
